f18c84f7e1b7bf88082305c8b871269a43c1750e
eldar <eldar_halilovic@live.com>
2022-11-16 18:44:08 +0100

    Executor sa main funkcijom kreiran

--- src/two/Executor.java
+++ src/two/Executor.java
@@ -1,5 +1,17 @@
 package two;
 
+/**
+ * Dvije vrste programskih jezika:
+ * <li>1. dynamically type lang</li>
+ *  JavaScript, Python, Pnp..
+ * <li>2. statically type lang</li>
+ *  Java, C#, Dart, Kotlin
+ *
+ *  <p>
+ *      Tri stvari tvore jedan izraz u programiranju.
+ *      TIP ime_Varijable = vrijednost;
+ *  </p>
+ */
 public class Executor {
     public static void main(String[] args) {
 
