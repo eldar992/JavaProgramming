6c026aadbc0ca183fff6d6c82a8a31dd197fce0e
eldar <eldar_halilovic@live.com>
2022-11-17 13:35:48 +0100

    Executor sa main funkcijom kreiran

--- src/two/Executor.java
+++ src/two/Executor.java
@@ -14,6 +14,21 @@ package two;
  */
 public class Executor {
     public static void main(String[] args) {
-
+        //byte - 8 bita
+        byte prviBroj = -128;
+        //short - 16 bit
+        short drugiBroj = -32768;
+        //int - 32 bit
+        int treciBroj = 327690000;
+        //long 64 bit
+        long cetvriBroj = 3276900000L;
+        //float 32
+        float prviBrojDecimalni = 23.4F;
+        //double 64
+        double drugiBrojDecimalni = 23.4;
+        //char
+        char slovo = 'A';
+        //boolean
+        boolean condition = false;
     }
 }
---
+++ src/two/UnaryDemo.java
@@ -0,0 +1,16 @@
+package two;
+
+/**
+ * Unarni operatori:
+ * <li>1. +</li>
+ * <li>2. -</li>
+ * <li>3. ++</li>
+ */
+public class UnaryDemo {
+    public static void main(String[] args) {
+    int number = -23;
+        System.out.println(number);
+        number = -number;
+        System.out.println(number);
+    }
+}
